936aec0a11ab1c9c99f0861d4486220073f0ea54
Elziekio <anthony.deamer@hotmail.com>
2021-06-27 19:30:52 +1000

    updated Java Script

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,72 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-var confirmNumber;
+var confirmNumber = 0;
+var confirmUppercase;
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var charaters = "!@#$%^&*()_-=+?<>/";
+var numbers = "0123456789"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordText = 0;
 
 // Write password to the #password input
 function writePassword() {
- window.prompt("Enter how many characters: Must be between 8 and 128 characters");
- if (confirmNumber > 8 && confirmNumber < 128){
-  window.alert("Must be between 8 and 128 charaters")
- };
+ confirmNumber = +prompt("Enter how many characters: Must be between 8 and 128 characters");
+ console.log(confirmNumber);
+ if (confirmNumber < 8 || confirmNumber > 128){
+  result = alert("Must be between 8 and 128 charaters")
+ }else 
 
- 
+ var passwordText = document.querySelector('#password');
+ passwordText.value = password;
+};
 
- 
-  var passwordText = document.querySelector('#password');
+//function for upper case popup
+function writePassword2 () {
+  alert("Do you want upper use upper case letters?")
+  if (true){
+  uppercase = math.random;
+  console.log(uppercase);
+  }else{}
+};
 
+//function for lower case popup
+function writePassword3 () {
+  alert("Do you want to use lowercase letters?")
+  if (true){
+  leters = math.random;
+  }else{}
+};
+
+//function for symbols popup
+function writePassword4 () {
+  alert("Do you want to use symbols?")
+  if (true){
+  charcters = math.random;
+  }else{}
+};
+
+//function for numbers popup
+function writePassword5 () {
+  alert("Do you want to use numbers?")
+  if(true){
+    numbers = math.random;
+  }else {}
+};
+
+//for loop for 
+ for (var i = 0; i<=passwordText; i++){
+  
+ }
+
+ //add pasword to display
+ document.getElementById('card-body').value = password;
+  var passwordText = document.querySelector('#password').value;
   passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+// Add event listener to generate button/ calling functions
+generateBtn.addEventListener('click', writePassword)
 
+writePassword2;
+writePassword3;
+writePassword4;
+writePassword5;
